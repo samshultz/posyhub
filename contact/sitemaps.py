2ab7b48b6874e7769176e0c00e546dee1ab10a1e
Samuel Taiwo <taiwogabrielsamuel@gmail.com>
2018-12-01 10:01:58 +0100

    removed contact app

--- contact/sitemaps.py
+++ contact/sitemaps.py
@@ -1,14 +1,14 @@
-from django.contrib.sitemaps import Sitemap
-from django.urls import reverse
-from .models import CompanyDetail
+# from django.contrib.sitemaps import Sitemap
+# from django.urls import reverse
+# from .models import CompanyDetail
 
 
-class ContactSitemap(Sitemap):
-    changefreq = 'monthly'
-    priority = 0.9
+# class ContactSitemap(Sitemap):
+#     changefreq = 'monthly'
+#     priority = 0.9
   
-    def items(self):
-        return CompanyDetail.objects.all() #NOQA
+#     def items(self):
+#         return CompanyDetail.objects.all() #NOQA
 
-    def location(self, obj):
-        return reverse("contact:contact-us")
+#     def location(self, obj):
+#         return reverse("contact:contact-us")
